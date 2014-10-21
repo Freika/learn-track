23ee3b270b771c5be0db8f27a61cb75a4408d435
Freika <iamfrey@gmail.com>
2014-10-21 23:18:42 +0700

    Added: lil controller spec

--- spec/controllers/knowledges_controller_spec.rb
+++ spec/controllers/knowledges_controller_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 RSpec.describe KnowledgesController, :type => :controller do
 
+  describe "PUT update" do
+    let(:admin) {create :admin}
+    it "updates knowledge" do
+      sign_in admin
+      @knowledge = create(:knowledge)
+      put :update, id: @knowledge, knowledge: attributes_for(:knowledge, name: "Test")
+      @knowledge.reload
+
+      expect(@knowledge.name).to eq "Test"
+    end
+  end
+
   describe "GET index" do
-    it "returns http success" do
+    it "lists all existing knowledges" do
       get :index
-      expect(response).to be_success
+      # Переписать так, чтобы отвечало названию спека
+      expect(response.body).to render_template :index
     end
   end
 
